659d951a04a79ae54958fdf08efa74ba4a9a67ee
Linda Bengtsson <linda-bengsson@outlook.com>
2025-01-17 18:42:35 +0100

    forgot password function added

--- script.js
+++ script.js
@@ -2,29 +2,36 @@ var login = document.querySelector(".signin-switch");
 var register = document.querySelector(".signup-switch");
 var box_login = document.querySelector(".login-box");
 var box_signup = document.querySelector(".signup-box");
+var forgot_link = document.querySelector(".forgot-link");
+var box_forgot = document.querySelector(".forgot-box");
 
+// Sign up switch button clicked //
 register.addEventListener("click", function () {
   box_login.classList.add("hidden");
   box_signup.classList.remove("hidden");
   box_signup.classList.add("active");
+  box_forgot.classList.remove("active");
+  box_forgot.classList.add("hidden");
+  login.classList.remove("active");
+  register.classList.add("active");
 });
 
+// Log in switch button clicked //
 login.addEventListener("click", function () {
   box_login.classList.remove("hidden");
   box_login.classList.add("active");
   box_signup.classList.remove("active");
   box_signup.classList.add("hidden");
+  box_forgot.classList.remove("active");
+  box_forgot.classList.add("hidden");
+  login.classList.add("active");
+  register.classList.remove("active");
 });
 
-var Lswitch = document.querySelector(".signin-switch");
-var Rswitch = document.querySelector(".signup-switch");
-
-Lswitch.addEventListener("click", function () {
-  Lswitch.classList.add("active");
-  Rswitch.classList.remove("active");
-});
-
-Rswitch.addEventListener("click", function () {
-  Rswitch.classList.add("active");
-  Lswitch.classList.remove("active");
+// Forgot password link clicked//
+forgot_link.addEventListener("click", function () {
+  box_forgot.classList.remove("hidden");
+  box_forgot.classList.add("active");
+  box_login.classList.remove("active");
+  box_login.classList.add("hidden");
 });
